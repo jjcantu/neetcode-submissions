1a4e7dc215285f089931ad10c43185bfea83b4ed
Jose Cantu <josecantu_20@yahoo.com>
2026-07-19 23:57:06 -0500

    Add: rotting-fruit - submission-0

---
+++ Data Structures & Algorithms/rotting-fruit/submission-0.js
@@ -0,0 +1,67 @@
+class Solution {
+    /**
+     * @param {number[][]} grid
+     * @return {number}
+     */
+    orangesRotting(grid) {
+        const queue = [];
+        const ROWS = grid.length;
+        const COLS = grid[0].length;
+        const visited = new Set();
+        let elapsedMinutes = 0;
+
+        const addCell = (r, c) => {
+            if (
+                r < 0 || 
+                r >= ROWS || 
+                c < 0 || 
+                c >= COLS || 
+                visited.has(`${r},${c}`)
+                ) {
+                return;
+            }
+
+            if (grid[r][c] !== 1) {
+                return;
+            }
+
+            grid[r][c] = 2;
+            queue.push([r,c])
+            visited.add(`${r},${c}`)
+        }
+
+        for (let r = 0; r < ROWS; r++) {
+            for (let c = 0; c < COLS; c++) {
+                if (grid[r][c] === 2) {
+                    queue.push([r, c]);
+                    visited.add(`${r},${c}`)
+                }
+            }
+        }
+
+        while (queue.length) {
+            const queueLength = queue.length;
+            for (let i = 0; i < queueLength; i++) {
+                const [r, c] = queue.shift() 
+                addCell(r + 1, c);
+                addCell(r - 1, c);
+                addCell(r, c + 1);
+                addCell(r, c - 1);
+            }
+
+            if (queue.length) {
+                elapsedMinutes++;
+            }
+        }
+
+        for (let r = 0; r < ROWS; r++) {
+            for (let c = 0; c < COLS; c++) {
+                if (grid[r][c] === 1) {
+                    return -1
+                }
+            }
+        }
+
+        return elapsedMinutes;
+    }
+}
