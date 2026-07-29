88c346990e6841fa199fca79e1b1fb4eb0f4f0bc
Jose Cantu <josecantu_20@yahoo.com>
2026-07-28 21:56:28 -0500

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {boolean}
+     */
+    validTree(n, edges) {
+        if (!n) return true;
+
+        const adjList = Array.from({ length: n}, () => []);
+        const visited = new Set();
+
+        for (let [node1, node2] of edges) {
+            adjList[node1].push(node2);
+            adjList[node2].push(node1);
+        }
+
+        const dfs = (node, prevNode) => {
+            if (visited.has(node)) {
+                return false
+            }
+
+            visited.add(node)
+
+            for (let neighbor of adjList[node]) {
+                if (neighbor === prevNode) continue;
+
+                if (dfs(neighbor, node) === false) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        return dfs(0, -1) && visited.size === n
+    }
+}
